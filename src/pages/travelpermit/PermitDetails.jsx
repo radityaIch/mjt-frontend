022de8db473eaefc9e208115edd6f235438adefd
radityaIch <raditya2678@gmail.com>
2022-10-10 05:51:06 +0800

    Create permit details page

---
+++ src/pages/travelpermit/PermitDetails.jsx
@@ -0,0 +1,25 @@
+import { Link, useParams } from "react-router-dom";
+import TravelForm from "./TravelForm";
+
+const PermitDetails = () => {
+  const { id } = useParams();
+
+  return (
+    <>
+      <div className="btn-group mb-1">
+        <Link to="#" className="btn btn-secondary fw-light active">
+          Detail Surat Jalan
+        </Link>
+        <Link
+          to={`/dashboard/surat-jalan/${id}/update-perjalanan`}
+          className="btn bg-white fw-light"
+        >
+          Update Perjalanan
+        </Link>
+      </div>
+      <TravelForm isEdit />
+    </>
+  );
+};
+
+export default PermitDetails;
